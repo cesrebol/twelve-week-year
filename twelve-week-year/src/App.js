dce00803d574263f5312425a7cac1c41551aa8be
Cesar Rebolledo <rebolledoces@gmail.com>
2023-09-19 14:40:21 -0400

    created two components

--- twelve-week-year/src/App.js
+++ twelve-week-year/src/App.js
@@ -5,8 +5,25 @@ function App() {
   return (
     <div className="App">
 
+      <h1>Twelve Week Year App</h1>
+      <MyButton />
+      <AddText />
+
     </div>
   );
 }
 
+function MyButton() {
+  return (
+    <button>Add Goal</button>
+
+  )
+  
+}
+
+function AddText() {
+  return (
+    <input type="text" name="goal" value="goal"></input>
+  )
+}
 export default App;
