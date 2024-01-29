727e18434095f7be8511974de7547c467292e30e
Cesar Rebolledo <rebolledoces@gmail.com>
2024-01-29 14:58:14 -0500

    Added the Aspirational component

---
+++ src/pages/Aspirational/Aspirational.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Aspirational.css';
+
+const Aspirational = () => {
+    <div className="aspirationa">
+        
+    </div>
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Home.css';
+import './Home.css'; // import the .css file 
 
 const Home = () => {
     return (
