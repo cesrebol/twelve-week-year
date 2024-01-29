b04b08ebf192397784badcbd526594f742a2e3ad
Cesar Rebolledo <rebolledoces@gmail.com>
2024-01-29 14:18:31 -0500

    Clean up dir tree

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to Twelve-Week-Year</h1>
+            <p>This app is designed to help you achieve your year's goal in just 12 weeks!</p>
+            <p>Get ready to boost your productivity.</p>
+        </div>
+    )
+}
+
+
+export default Home;
