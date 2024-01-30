b94e79cf8700c9379a4658573bdc75a4b4b3498f
Cesar Rebolledo <rebolledoces@gmail.com>
2024-01-30 09:09:50 -0500

    rebase purpose

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css'; // import the .css file 
+import Aspirational from '../Aspirational/Aspirational';
 
 const Home = () => {
     return (
@@ -7,6 +8,8 @@ const Home = () => {
             <h1>Welcome to Twelve-Week-Year</h1>
             <p>This app is designed to help you achieve your year's goal in just 12 weeks!</p>
             <p>Get ready to boost your productivity.</p>
+
+            <Aspirational />
         </div>
     )
 }
